fca2817164dc7c76477d59808ced2a94ce5e8371
met43211 <matvey-anashkin@yandex.ru>
2024-06-13 10:38:02 +0300

    fix: conflicts in project version

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { HomePage } from '@/src/page/home';
-import { PageProps } from '@/src/shared/model/default.type';
 
-export default function Home({ searchParams }: PageProps) {
-  return <HomePage searchParams={searchParams} />;
+export default function Home() {
+  return <HomePage />;
 }
