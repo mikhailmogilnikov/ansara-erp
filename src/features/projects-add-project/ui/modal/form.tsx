612c9e8f6e67f64237c397ce6a58a5f52896a71b
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-06-25 14:39:42 +0300

    fix: date picker

--- src/features/projects-add-project/ui/modal/form.tsx
+++ src/features/projects-add-project/ui/modal/form.tsx
@@ -6,41 +6,62 @@ import { InputLabel } from '@/src/shared/ui/(inputs)/input-label';
 import { CurrencyInput } from '@/src/shared/ui/(inputs)/currency-input';
 import { SelectInput } from '@/src/shared/ui/(inputs)/select-input';
 import { PasswordInput } from '@/src/shared/ui/(inputs)/password-input';
+import { DatePickerInput } from '@/src/shared/ui/(inputs)/date-picker';
 
 export const AddProjectForm = () => {
-  const [stages, setStages] = useState(['1']);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [stages, setStages] = useState(['2']);
   const [accounters, setAccounters] = useState(['1']);
   const [payed, setPayed] = useState(0);
   const [needPay, setNeedPay] = useState(0);
+  const [startDate, setStartDate] = useState<Date>(new Date());
 
   return (
-    <form className='grid grid-cols-2 gap-4 gap-y-6 mt-2'>
+    <form autoComplete='off' className='grid grid-cols-2 gap-4 gap-y-6 mt-2'>
       <Input
         classNames={{ inputWrapper: '!bg-default' }}
         label='Заказчик'
         labelPlacement='outside'
         placeholder='Имя Ниша'
         size='lg'
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
 
       <InputLabel title='Телефон'>
-        <PhoneInput className='w-full bg-default rounded-[14px] h-12 px-2 outline-none shadow-sm' />
+        <PhoneInput
+          className='w-full bg-default rounded-[14px] h-12 px-2 outline-none shadow-sm'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </InputLabel>
 
       <Input
+        autoComplete='off'
         classNames={{ inputWrapper: '!bg-default' }}
         label='Логин ЛК'
         labelPlacement='outside'
+        name='login123'
         placeholder='Введите логин'
         size='lg'
+        type='text'
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
       />
 
       <PasswordInput
+        autoComplete='off'
         classNames={{ inputWrapper: '!bg-default' }}
         label='Пароль ЛК'
         labelPlacement='outside'
         placeholder='Введите пароль'
         size='lg'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <InputLabel title='Количество этапов'>
@@ -74,6 +95,10 @@ export const AddProjectForm = () => {
 
       <InputLabel title='Осталось'>
         <CurrencyInput setValue={setNeedPay} value={needPay} />
+      </InputLabel>
+
+      <InputLabel title='Дата первой оплаты'>
+        <DatePickerInput date={startDate} size='lg' onChange={setStartDate} />
       </InputLabel>
     </form>
   );
