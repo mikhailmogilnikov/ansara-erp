32310f94d9eb33367b7e5108484af6ec39b89e15
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-06-25 12:11:07 +0300

    fix: types

--- src/entities/projects-table-element/tasks/config/test-tasks.ts
+++ src/entities/projects-table-element/tasks/config/test-tasks.ts
@@ -1,4 +1,5 @@
-import { TTableTask } from '../../model/task.type';
+import { TTableTask } from "../model/task.type";
+
 
 export const TableTestTasks: TTableTask[] = [
   { id: 0, name: 'Задача 1', isCompleted: true, order: 0 },
