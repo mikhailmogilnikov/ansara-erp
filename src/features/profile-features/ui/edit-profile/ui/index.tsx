335f6a6f12cdf83904a06177fb91a07e09c07299
met43211 <matvey-anashkin@yandex.ru>
2024-07-04 17:13:46 +0300

    feat: change avatar

--- src/features/profile-features/ui/edit-profile/ui/index.tsx
+++ src/features/profile-features/ui/edit-profile/ui/index.tsx
@@ -1,8 +1,6 @@
-import { PiPencilSimpleBold, PiUserBold } from 'react-icons/pi';
+import { PiPencilSimpleBold } from 'react-icons/pi';
+import { ChangeEventHandler, useRef, useState } from 'react';
 
-import { EditProfileModal } from './modal';
-
-import { useModal } from '@/src/shared/ui/modal';
 import { Text } from '@/src/shared/ui/(layout)/text';
 
 interface Props {
@@ -10,20 +8,33 @@ interface Props {
 }
 
 export const EditProfileButton = ({ closeModal }: Props) => {
-  const { setModal } = useModal();
+  const [avatar, setAvatar] = useState<File>();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleChangeEmployees = () => {
-    closeModal();
-    setModal(<EditProfileModal />);
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { files } = e.target;
+
+    if (files) {
+      setAvatar(files[0]);
+    }
   };
 
   return (
-    <button
-      className='px-2 h-12 rounded-2xl flex gap-3 items-center active:scale-95 transition-all outline-none'
-      onClick={handleChangeEmployees}
-    >
-      <PiPencilSimpleBold className='w-auto h-1/2 text-white' />
-      <Text className='text-white font-medium'>Изменить профиль</Text>
-    </button>
+    <>
+      <button
+        className='px-2 h-12 rounded-2xl flex gap-3 items-center active:scale-95 transition-all outline-none'
+        onClick={handleChangeEmployees}
+      >
+        <PiPencilSimpleBold className='w-auto h-1/2 text-white' />
+        <Text className='text-white font-medium'>Изменить аватар</Text>
+      </button>
+      <input ref={fileInputRef} className='hidden' type='file' onChange={handleInputChange} />
+    </>
   );
 };
